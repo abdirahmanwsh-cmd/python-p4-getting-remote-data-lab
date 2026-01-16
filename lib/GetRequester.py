4723b19c757d1d4c42272388a1fa562c70a83b24
Manka.code <Abdirahman.wsh@gmail.com>
2026-01-16 16:59:25 +0300

    adds getrequest.py

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 class GetRequester:
 
@@ -7,7 +8,17 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        max_retries = 3
+        for attempt in range(max_retries):
+            try:
+                response = requests.get(self.url)
+                return response.content
+            except requests.exceptions.ConnectionError:
+                if attempt < max_retries - 1:
+                    time.sleep(1)  # Wait 1 second before retrying
+                else:
+                    raise
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
